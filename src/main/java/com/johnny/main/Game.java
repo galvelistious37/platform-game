5654ac7ef7e53294af631167e5efdcde14be24f2
johnny <john.robertson88@yahoo.com>
2024-03-09 23:14:43 -0800

    Open a plain JFrame window and end the program on close

---
+++ src/main/java/com/johnny/main/Game.java
@@ -0,0 +1,10 @@
+package com.johnny.main;
+
+public class Game {
+    private GameWindow gameWindow;
+
+    public Game() {
+        gameWindow = new GameWindow();
+    }
+
+}
---
+++ src/main/java/com/johnny/main/GameWindow.java
@@ -0,0 +1,12 @@
+package com.johnny.main;
+
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+
+    public GameWindow() {
+        setSize(400, 400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
